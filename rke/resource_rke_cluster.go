d74fe8d2b8efe7ac9a58d67de622d4a7223ee78f
febc.yamamoto <yamamoto.febc@gmail.com>
2018-06-25 23:26:49 +0900

    add rke_node_parameter data source

--- rke/resource_rke_cluster.go
+++ rke/resource_rke_cluster.go
@@ -34,100 +34,24 @@ func resourceRKECluster() *schema.Resource {
 				Optional: true,
 				Default:  false,
 			},
+			"nodes_conf": {
+				Type:          schema.TypeList,
+				MinItems:      1,
+				Optional:      true,
+				Description:   "Kubernetes nodes(YAML or JSON)",
+				Elem:          &schema.Schema{Type: schema.TypeString},
+				ConflictsWith: []string{"nodes"},
+			},
 			"nodes": {
 				Type:        schema.TypeList,
-				Required:    true,
 				MinItems:    1,
+				Optional:    true,
 				Description: "Kubernetes nodes",
 				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"node_name": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Computed:    true,
-							Description: "Name of the host provisioned via docker machine",
-						},
-						"address": {
-							Type:        schema.TypeString,
-							Required:    true,
-							Description: "IP or FQDN that is fully resolvable and used for SSH communication",
-						},
-						"port": {
-							Type:         schema.TypeInt,
-							Optional:     true,
-							Computed:     true,
-							ValidateFunc: validateIntegerInRange(1, 65535),
-							Description:  "Port used for SSH communication",
-						},
-						"internal_address": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Computed:    true,
-							Description: "Internal address that will be used for components communication",
-						},
-						"role": {
-							Type:          schema.TypeList,
-							Elem:          &schema.Schema{Type: schema.TypeString},
-							Optional:      true,
-							// cannot use ConflictsWith in this context. see https://github.com/terraform-providers/terraform-provider-google/pull/1062
-							// ConflictsWith: []string{"roles"},
-							Description:   "Node role in kubernetes cluster [controlplane/worker/etcd])",
-						},
-						"roles": {
-							Type:          schema.TypeString,
-							Optional:      true,
-							// cannot use ConflictsWith in this context. see https://github.com/terraform-providers/terraform-provider-google/pull/1062
-							// ConflictsWith: []string{"role"},
-							Deprecated:    "roles is a workaround when a role can not be specified in list",
-							Description:   "Node role in kubernetes cluster [controlplane/worker/etcd], specified by a comma-separated string",
-						},
-						"hostname_override": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Computed:    true,
-							Description: "HostnameOverride",
-						},
-						"user": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Computed:    true,
-							Description: "SSH user that will be used by RKE",
-						},
-						"docker_socket": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Computed:    true,
-							Description: "Docker socket on the node that will be used in tunneling",
-						},
-						"ssh_agent_auth": {
-							Type:        schema.TypeBool,
-							Optional:    true,
-							Computed:    true,
-							Description: "SSH Agent Auth enable",
-						},
-						"ssh_key": {
-							Type:        schema.TypeString,
-							Sensitive:   true,
-							Optional:    true,
-							Computed:    true,
-							Description: "SSH Private Key",
-						},
-						"ssh_key_path": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Computed:    true,
-							Description: "SSH Private Key",
-						},
-						"labels": {
-							Type:        schema.TypeMap,
-							Optional:    true,
-							Computed:    true,
-							Description: "Node Labels",
-						},
-					},
+					Schema: nodeSchema(),
 				},
+				ConflictsWith: []string{"nodes_conf"},
 			},
-
 			"services_etcd": {
 				Type:     schema.TypeList,
 				MaxItems: 1,
