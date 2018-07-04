c77ce4321db456608e9025df67335c0cf18da658
febc.yamamoto <yamamoto.febc@gmail.com>
2018-07-04 11:18:09 +0900

    Improve warning/error log output from RKE

--- rke/provider.go
+++ rke/provider.go
@@ -1,13 +1,17 @@
 package rke
 
 import (
-	"io/ioutil"
+	"bytes"
+	"fmt"
+	"io"
 	"os"
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
 	"github.com/sirupsen/logrus"
 )
+
+var rkeLogBuf = &bytes.Buffer{}
 
 // Provider returns a terraform.ResourceProvider.
 func Provider() terraform.ResourceProvider {
@@ -30,12 +34,26 @@ func Provider() terraform.ResourceProvider {
 }
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
-	logrus.SetOutput(ioutil.Discard)
-	if v, ok := d.GetOk("log"); ok {
-		if v.(bool) {
-			logrus.SetOutput(os.Stderr)
-		}
-	}
+	initLogger(d)
+
 	config := Config{}
 	return config, nil
+}
+
+func initLogger(d *schema.ResourceData) {
+	var writer io.Writer = rkeLogBuf
+	if v, ok := d.GetOk("log"); ok {
+		if v.(bool) {
+			writer = io.MultiWriter(os.Stderr, rkeLogBuf)
+		}
+	}
+
+	logrus.SetOutput(writer)
+	logrus.SetFormatter(&logFormatter{})
+}
+
+type logFormatter struct{}
+
+func (l *logFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	return []byte(fmt.Sprintf("[%s] %s\n", entry.Level, entry.Message)), nil
 }
