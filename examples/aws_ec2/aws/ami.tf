0174adab99c96f8ea4ec6ffe7950b0a7a3ae5b6d
Ajdin Lokvancic <ajdin@cloudnovi.com>
2020-10-02 13:37:49 +0200

    Change to ubuntu ami 18.04

--- examples/aws_ec2/aws/ami.tf
+++ examples/aws_ec2/aws/ami.tf
@@ -3,8 +3,8 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
-  }
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+}
 
   filter {
     name   = "virtualization-type"
