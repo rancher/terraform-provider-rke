9f9e67292708451cda8361877e95e10709e2d067
J. Adam Craig <adam@jadamcraig.com>
2020-12-29 19:44:37 -0500

    Removed KubeletPluginsWatcher=true from kube_api and kubelet feature-gates, since declaring this hasn't been allowed since K8s v1.16

--- examples/digitalocean/rke.tf
+++ examples/digitalocean/rke.tf
@@ -45,13 +45,13 @@ resource "rke_cluster" "cluster" {
     kube_api {
       extra_args = {
         kubelet-preferred-address-types = "InternalIP,ExternalIP,Hostname"
-        feature-gates = "KubeletPluginsWatcher=true,CSINodeInfo=true,CSIDriverRegistry=true"
+        feature-gates = "CSINodeInfo=true,CSIDriverRegistry=true"
       }
     }
     kubelet {
       extra_args = {
         cloud-provider = "external"
-        feature-gates = "KubeletPluginsWatcher=true,CSINodeInfo=true,CSIDriverRegistry=true"
+        feature-gates = "CSINodeInfo=true,CSIDriverRegistry=true"
       }
     }
   }
